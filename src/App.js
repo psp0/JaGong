7bd3859e0651813b82b8af8849101d197e1ebc26
psp0 <tkdvlf9058@g.hongik.ac.kr>
2023-06-28 20:09:10 +0900

    카카오 지도 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import ApiTest from "./components/ApiTest";
+import Map from "./components/Map/Map";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Map />
         <ApiTest />
       </header>
     </div>
