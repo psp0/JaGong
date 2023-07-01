773f128e3bf4b9ea8174a5fa3ae6c29f774a7bf0
psp0 <tkdvlf9058@g.hongik.ac.kr>
2023-07-01 23:25:58 +0900

    지도 크기 수정

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import "./Map.css";
 const SERVICE_KEY = process.env.REACT_APP_SEONGNAM_BIKE_API_KEY;
 const Map = () => {
   const [SN, SetSN] = useState({ data: [] });
@@ -22,42 +23,41 @@ const Map = () => {
           console.log(err);
         });
     }
-    if (kakao !== undefined && SN.data.length) {
-      var mapContainer = document.getElementById("map"), // 지도를 표시할 div
-        mapOption = {
-          center: new kakao.maps.LatLng(37.54699, 127.09598), // 지도의 중심좌표
-          level: 4, // 지도의 확대 레벨
-        };
-      var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
-
-      var imageSrc =
-          "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png", // 마커이미지의 주소입니다
-        imageSize = new kakao.maps.Size(64, 69), // 마커이미지의 크기입니다
-        imageOption = { offset: new kakao.maps.Point(27, 69) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
-
-      SN.data.forEach((data) => {
-        // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
-        var markerImage = new kakao.maps.MarkerImage(
-            imageSrc,
-            imageSize,
-            imageOption
-          ),
-          markerPosition = new kakao.maps.LatLng(data.위도, data.경도); // 마커가 표시될 위치입니다
-
-        // 마커를 생성합니다
-        var marker = new kakao.maps.Marker({
-          position: markerPosition,
-          image: markerImage, // 마커이미지 설정
-        });
-        // 마커가 지도 위에 표시되도록 설정합니다
-        marker.setMap(map);
-      });
-    }
   }, [SN]);
+  if (kakao !== undefined && SN.data.length) {
+    var mapContainer = document.getElementById("map"), // 지도를 표시할 div
+      mapOption = {
+        center: new kakao.maps.LatLng(37.3794, 127.1129), // 지도의 중심좌표
+        level: 5, // 지도의 확대 레벨
+      };
+    var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+
+    var imageSrc =
+        "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png", // 마커이미지의 주소입니다
+      imageSize = new kakao.maps.Size(64, 69), // 마커이미지의 크기입니다
+      imageOption = { offset: new kakao.maps.Point(27, 69) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+
+    SN.data.forEach((data) => {
+      // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+      var markerImage = new kakao.maps.MarkerImage(
+          imageSrc,
+          imageSize,
+          imageOption
+        ),
+        markerPosition = new kakao.maps.LatLng(data.위도, data.경도); // 마커가 표시될 위치입니다
+
+      // 마커를 생성합니다
+      var marker = new kakao.maps.Marker({
+        position: markerPosition,
+        image: markerImage, // 마커이미지 설정
+      });
+      // 마커가 지도 위에 표시되도록 설정합니다
+      marker.setMap(map);
+    });
+  }
   return (
     <>
-      {/* <script type="text/javascript" src={src}></script> */}
-      <div id="map" style={{ width: "500px", height: "400px" }}></div>
+      <div id="map"></div>
     </>
   );
 };
