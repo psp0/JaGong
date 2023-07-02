c07816c42883c05dfe5e3a232cb0d677c7623821
psp0 <tkdvlf9058@g.hongik.ac.kr>
2023-07-02 22:33:02 +0900

    지도내 인포창 생성

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -15,7 +15,6 @@ const Map = () => {
           return res.json();
         })
         .then((value) => {
-          console.log(value);
           if (value.status !== undefined) SetSN(value);
           else SetSN(value);
         })
@@ -50,9 +49,26 @@ const Map = () => {
       var marker = new kakao.maps.Marker({
         position: markerPosition,
         image: markerImage, // 마커이미지 설정
+        clickable: true,
       });
       // 마커가 지도 위에 표시되도록 설정합니다
       marker.setMap(map);
+
+      // 마커를 클릭했을 때 마커 위에 표시할 인포윈도우를 생성합니다
+      var iwContent = `<div class="text" style="padding:5px;">${data["공기주입기 종류"]}</div>`, // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+        iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
+
+      // 인포윈도우를 생성합니다
+      var infowindow = new kakao.maps.InfoWindow({
+        content: iwContent,
+        removable: iwRemoveable,
+      });
+
+      // 마커에 클릭이벤트를 등록합니다
+      kakao.maps.event.addListener(marker, "click", function () {
+        // 마커 위에 인포윈도우를 표시합니다
+        infowindow.open(map, marker);
+      });
     });
   }
   return (
